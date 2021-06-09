24f1c5be835b436df8805314aff9e612f43a5399
caiyuntao <960269915@qq.com>
2021-06-09 17:26:02 +0800

    1

---
+++ miniprogram/app.js
@@ -0,0 +1,26 @@
+//app.js
+// npm install --save wx-server-sdk
+App({
+  onLaunch: function () {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      this.globalData = {
+        isPro:__wxConfig.envVersion //develop
+      }
+      wx.cloud.init({
+        // env 参数说明：
+        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
+        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
+        //   如不填则使用默认环境（第一个创建的环境）
+        env: __wxConfig.envVersion == 'develop' ? 'dev-7gau4e4k4b9e8a9f' : 'pro-8ganrobg1159dc22'
+      })
+
+      wx.cloud.callFunction({
+        name:"login"
+      }).then(res=>{
+        console.log(res);
+      })
+    }
+  }
+})
